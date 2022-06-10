59edc2664b2423678fd81f31cefeb45c5b0119db
Dtuck37 <104952981+Dtuck37@users.noreply.github.com>
2022-06-10 10:41:59 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// String//
+console.log("Hello World")
+// Number//
+console.log(10)
+// Boolean//
+let a = true
+//null/
+let b = null
+// Undefine//
+let c;
+// Object //
+let d = { make :"Toyota", model:"Highlander", year:"2003"}
+// Variable //
+let userName = "Derick Tucker Jr."
+// Variable Use Case //
+console.log(userName)
+// Sales //
+let phone = 1500
+let finalPrice = phone+(phone * .25)
+console.log(phone)
+console.log(finalPrice)
+// String Concatenation//
+console.log("my user name is " + userName)
+// String Interpolation //
+console.log(`my user name is: ${userName}`)
+// Array //
+let dragonBallCharater = ["Goku", "Gohan", "Piccolo", "Chi-Chi", "Trucks", "Gotenks", "Goten", "Pan", "Vageta", "Radis"]
+// Index from Array find Trucks //
+console.log(dragonBallCharater [4])
